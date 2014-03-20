ab79d930a997fd3a6707e01a1eab506bcb41c24f
Justin Potts <justinpotts98@gmail.com>
2014-03-20 18:02:00 -0500

    Created a test to make sure sample makes were displayed on the home page

--- pages/home.py
+++ pages/home.py
@@ -4,13 +4,20 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from selenium.webdriver.common.by import By
+
 from base import Base
 
 
 class HomePage(Base):
 
     _page_title = 'Home - Mozilla Webmaker'
+    _makes_templates_locator = (By.CSS_SELECTOR, 'DIV.make-now-templates')
 
     def go_to_page(self):
         self.selenium.get(self.base_url + '/')
         self.is_the_current_page
+
+    @property
+    def makes_templates_present(self):
+        return self.is_element_present(*self._makes_templates_locator)
---
+++ tests/test_makes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+from unittestzero import Assert
+
+from pages.home import HomePage
+
+
+class TestMakes:
+
+    @pytest.mark.nondestructive
+    def test_sample_makes_are_visible_on_home_page(self, mozwebqa):
+        home_page = HomePage(mozwebqa)
+        home_page.go_to_page()
+        Assert.true(home_page.makes_templates_present)
