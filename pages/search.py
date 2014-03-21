f0861ec89f57fc88ee872f553d4dfc61322e9c0a
mediocrity <kristiansson.tobias@gmail.com>
2014-03-21 12:45:23 +0100

    Fix search result locator.

--- pages/search.py
+++ pages/search.py
@@ -16,7 +16,7 @@ class Search(Page):
     _search_box_locator = (By.ID, 'search-field')
     _search_button_locator = (By.CSS_SELECTOR, 'button.search-btn')
     _search_query_word_locator = (By.CSS_SELECTOR, 'span.search-query-word')
-    _result_locator = (By.CSS_SELECTOR, 'DIV.main-gallery.search-gallery')
+    _result_locator = (By.CSS_SELECTOR, 'div.make-search')
 
     def search(self, search_term):
         element = self.selenium.find_element(*self._search_box_locator)
