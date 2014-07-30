b6e84dbe8d52de5bda386567c904ec4ec7561079
Amit Verma <amitverma12@gmail.com>
2014-07-30 00:33:09 -0400

    Added the test_explore_title
    Related to issue #48

---
+++ tests/test_explore.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+from unittestzero import Assert
+
+from pages.home import HomePage
+
+@pytest.mark.xfail(reason="Bug 1029294 - Page title for home page is incorrect")
+class TestExplorePage:
+
+    @pytest.mark.nondestructive
+    def test_explore_title(self, mozwebqa):
+        home_page = HomePage(mozwebqa)
+        home_page.go_to_page()
+        explore_page = home_page.header.click_on_explore_tab()
+        Assert.equal(explore_page._page_title, explore_page.page_title)
