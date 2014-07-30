d20ec592e15689679dfce284f52a6150c3bb2351
Amit Verma <amitverma12@gmail.com>
2014-07-30 00:32:36 -0400

    Added the explore page in pages
    Related to issue #48

---
+++ pages/explore.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from base import Base
+
+
+class ExplorePage(Base):
+
+    _page_title = u'Explore - Mozilla Webmaker'
+
+    def go_to_explore_page(self):
+        home_page = HomePage(mozwebqa)
+        home_page.go_to_page()
+
+        explore_page = home_page.header.click_on_explore_tab()
+        Assert.true(explore_page.is_the_current_page)
