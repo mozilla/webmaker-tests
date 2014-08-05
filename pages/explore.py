8ee934814d3a2cd7b66aee2920e75cf4d021e26a
Amit Verma <amitverma12@gmail.com>
2014-08-05 01:36:47 -0400

    Added is_page_title_visible to explore.py
    Added method in explore.py to verify that title locator is visible.

--- pages/explore.py
+++ pages/explore.py
@@ -4,14 +4,18 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from selenium.webdriver.common.by import By
 from base import Base
 
 
 class ExplorePage(Base):
 
-    _page_title = u'Explore - Mozilla Webmaker'
+    _explore_title_locator = (By.CSS_SELECTOR, 'title.ng-binding')
 
     def go_to_explore_page(self):
         home_page = HomePage(mozwebqa)
         home_page.go_to_page()
         explore_page = home_page.header.click_on_explore_tab()
+
+    def is_page_title_visible(self):
+        return self.is_element_visible(*self._explore_title_locator)
